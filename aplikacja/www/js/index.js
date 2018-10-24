c9667713e97c80fa95155597a724712c773327ce
Jakub Rurarz <kubarz94@gmail.com>
2018-10-24 17:09:42 +0200

    Add basic app layout, Add ajax requesting data from dummy json files

--- aplikacja/www/js/index.js
+++ aplikacja/www/js/index.js
@@ -16,6 +16,64 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+ $(document).ready(function() {
+
+
+ 	var filename = 'smog.json'
+
+	$('#sidebarCollapse').on('click', function () {
+		$('#sidebar').toggleClass('active');
+    });
+
+	$("#tomek").click( function(){
+		$("#tomasz").toggle()
+		});
+
+	//zmiana zrodla
+	$("#source1").click( function(){
+		filename = 'smog.json'
+		alert('Zmieniono źródło na 1');
+		});
+
+	$("#source2").click( function(){
+		filename = 'smog2.json'
+		alert('Zmieniono źródło na 2');
+		});
+
+	//funkcja wczytania danych, na razie z pliku
+	function load() {
+	  $.ajax({
+		  type: 'GET',
+		  url: filename,
+		  dataType: 'json',
+		  success: function(data) {
+
+			$(".smog-row").empty();
+
+			$.each(data.PM10, function(key, val) {
+				$("#smog-table").append("<tr class='smog-row'><td>" + val + "</td><td>" + data.O3[key] + "</td><td>" + data.SO2[key] + "</td><td>" + data.PM25[key] + "</td></tr>");
+			});
+		},
+      statusCode: {
+         404: function() {
+           alert('Could not load data');
+         }
+       }
+    });
+  }
+  
+	//uruchamiaj funkcje wczytania co 15s.
+	window.setInterval(function(){
+	  load()
+	  }, 15000);
+		
+	//uruchom funkcje wczytania guzikiem
+	$('#load').click(function() {
+       load()
+	});
+});
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -28,6 +86,7 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
+		$("#tomasz").hide()
     },
 
     // Update DOM on a Received Event
