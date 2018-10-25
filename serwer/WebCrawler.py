29df63352127b1c6a5dc320a45de2b6f4c566f1b
Wolffenstein <dominik.kozlowski.dk@gmail.com>
2018-10-25 22:24:46 +0200

    Add loading config when initializing WebCrawler

--- serwer/WebCrawler.py
+++ serwer/WebCrawler.py
@@ -1,5 +1,21 @@
+import json
+import os
+
+
 class WebCrawler:
+    def __init__(self):
+        self.__servers = None
+        self.loadConfigs()
+
     def start(self):
         print('Crawler Started.')
+
     def stop(self):
         print('Crawler Stopped.')
+
+    def loadConfigs(self):
+        dirname = os.path.dirname(__file__)
+        filename = os.path.join(dirname, 'configs\servers.json')
+        with open(filename) as dataFile:
+            self.__servers = json.load(dataFile)
+
--- serwer/main.py
+++ serwer/main.py
@@ -1,4 +1,7 @@
 from aiohttp import web
+from serwer.WebCrawler import WebCrawler
+
+webCrawler = WebCrawler()
 
 async def handle(request):
     return web.json_response(text="{}")
@@ -6,8 +9,11 @@ async def handle(request):
 async def defaultHandle(request):
     return web.Response(text="404: Not Found")
 
+async def on_shutdown(app):
+    webCrawler.stop()
+
 app = web.Application()
-app.add_routes([web.get('/', defaultHandle),
-                web.get('/getData', handle)])
+app.add_routes([web.get('/', defaultHandle), web.get('/getData', handle)])
+app.on_shutdown.append(on_shutdown(app))
 
 web.run_app(app, host="127.0.0.1", port=8080)
